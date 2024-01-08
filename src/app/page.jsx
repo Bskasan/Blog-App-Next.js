37a8922793d01cce05fc007398f3dea5b958a882
Bskasan <b.kasan@yandex.com>
2024-01-08 20:15:46 +0200

    up - footer and navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Link href="/">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tenetur ab accusamus quam provident ipsum dolor neque dolores, velit tempore!</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, assumenda aut velit, culpa delectus consequuntur molestiae neque iste deserunt exercitationem consequatur magnam ab odit, deleniti nemo earum laudantium illum quasi ea perferendis. Nesciunt rerum unde accusamus aperiam repudiandae, laudantium consequuntur debitis totam aliquam at quam eligendi amet! Veniam explicabo dolore debitis, odit velit quidem atque unde nisi, perspiciatis, laboriosam doloribus facilis iure minus eveniet deserunt recusandae tempora qui? Sequi, quia corporis doloremque ut architecto, labore doloribus minus alias tenetur aspernatur rem similique aut? Ipsam voluptate expedita est beatae quos obcaecati eveniet? In, non. Ab alias ipsam minima! Ipsum ex facere ab temporibus cupiditate, porro dolores incidunt accusantium pariatur quos eveniet natus praesentium modi sequi quidem suscipit iste commodi at eligendi consectetur? Nam incidunt consequatur accusantium neque! Ratione incidunt reiciendis aliquid sit deserunt ab magnam exercitationem dolore, itaque nesciunt eius quos hic temporibus error sapiente ipsum ex modi! Illo, amet, dicta saepe ipsa harum natus itaque asperiores adipisci architecto perspiciatis porro incidunt facere doloremque libero sequi explicabo ex expedita fugiat deserunt. Repudiandae accusamus enim, soluta excepturi ipsum veniam cum similique possimus repellendus exercitationem maxime consectetur ad quae laborum pariatur nemo officia at quibusdam! Illum, molestias repellat velit repudiandae earum recusandae eaque?</Link>
+      <Navbar />
+      <Footer />
     </div>
   );
 }
